578d5a2890c3586f73d35763df2faa6819d8d5ce
Oliwia9812 <90377006+Oliwia9812@users.noreply.github.com>
2022-03-16 13:49:58 +0100

    added portfolio section

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,4 @@
+//Nav
 const hamburgerMenuWrapper = document.querySelector('.header__nav-menu-icon-wrapper');
 const hamburgerMenu = document.querySelector('.header__nav-menu-icon'); 
 const menuNav = document.querySelector('.header__mobile-nav-menu');
@@ -12,3 +13,50 @@ hamburgerMenuWrapper.addEventListener('click', () => {
     });
 });
 
+// Scroll
+const scrollEl = document.querySelector('#horizontal-scroll');
+const dots = document.querySelectorAll('.portfolio__scroll-circle');
+const scrollImg = document.querySelectorAll('.portfolio__img');
+
+let portfolioImgWidth;
+let currentScrollPosition; 
+
+    scrollImg.forEach(el => {
+        portfolioImgWidth = el.offsetWidth;
+    });
+
+    scrollEl.addEventListener('scroll', () => {
+        currentScrollPosition = scrollEl.scrollLeft;
+        
+        if(currentScrollPosition < (portfolioImgWidth/2)){
+            removeActiveClass(1);
+            dots[0].classList.add('active');
+        } else if (currentScrollPosition > (portfolioImgWidth/2) && currentScrollPosition < (portfolioImgWidth+25+(portfolioImgWidth/2)) ){
+            removeActiveClass(0);
+            removeActiveClass(2);
+            dots[1].classList.add('active');
+        } else if (currentScrollPosition > (portfolioImgWidth+25+(portfolioImgWidth/2))&& currentScrollPosition < ((2*portfolioImgWidth)+25+(portfolioImgWidth/2))){
+            removeActiveClass(1);
+            removeActiveClass(3);
+            dots[2].classList.add('active');
+        } else if (currentScrollPosition > ((2*portfolioImgWidth)+25+(portfolioImgWidth/2)) && currentScrollPosition < ((3*portfolioImgWidth)+25+(portfolioImgWidth/2))){
+            removeActiveClass(2);
+            removeActiveClass(4);
+            dots[3].classList.add('active');
+        } else if (currentScrollPosition > ((3*portfolioImgWidth)+25+(portfolioImgWidth/2)) && currentScrollPosition < ((4*portfolioImgWidth)+25+(portfolioImgWidth/2))){
+            removeActiveClass(3);
+            removeActiveClass(5);
+            dots[4].classList.add('active');
+        }  else if (currentScrollPosition > ((4*portfolioImgWidth)+25+(portfolioImgWidth/2)) && currentScrollPosition < ((5*portfolioImgWidth)+25+(portfolioImgWidth/2))){
+            removeActiveClass(4);
+            removeActiveClass(6);
+            dots[5].classList.add('active');
+        }  else if (currentScrollPosition > ((5*portfolioImgWidth)+25+(portfolioImgWidth/2)) && currentScrollPosition < ((6*portfolioImgWidth)+25+(portfolioImgWidth/2))){
+            removeActiveClass(5);
+            dots[6].classList.add('active');
+        }  
+    });
+
+    function removeActiveClass(num) {
+        dots[num].classList.remove('active');
+    }
