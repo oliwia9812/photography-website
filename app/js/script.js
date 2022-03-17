87c4ed29eb03895fda7e6f005271b8f2b7610338
Oliwia9812 <90377006+Oliwia9812@users.noreply.github.com>
2022-03-17 14:38:49 +0100

    added feedback section

--- app/js/script.js
+++ app/js/script.js
@@ -1,4 +1,5 @@
-//Nav
+// Nav
+// ======
 const hamburgerMenuWrapper = document.querySelector('.header__nav-menu-icon-wrapper');
 const hamburgerMenu = document.querySelector('.header__nav-menu-icon'); 
 const menuNav = document.querySelector('.header__mobile-nav-menu');
@@ -13,7 +14,8 @@ hamburgerMenuWrapper.addEventListener('click', () => {
     });
 });
 
-// Scroll
+// Portfolio Scroll
+// ===================
 const scrollEl = document.querySelector('#horizontal-scroll');
 const dots = document.querySelectorAll('.portfolio__scroll-circle');
 const scrollImg = document.querySelectorAll('.portfolio__img');
@@ -60,3 +62,98 @@ let currentScrollPosition;
     function removeActiveClass(num) {
         dots[num].classList.remove('active');
     }
+
+// Feedback  carousel
+// =====================
+const prevIcon = document.querySelector('.feedback__arrow-icon--prev');
+const nextIcon = document.querySelector('.feedback__arrow-icon--next');
+const track = document.querySelector('.feedback__carousel-track');
+const slides = Array.from(track.children);
+const dotsNav = document.querySelector('.feedback__scroll-position');
+const dotsArray = Array.from(dotsNav.children);
+let slideWidth = slides[0].getBoundingClientRect().width;
+
+const moveToSlide = (track, currentSlide, targetSlide) => {
+    track.style.transform = 'translateX(-' + targetSlide.style.left; + ')';
+    currentSlide.classList.remove('feedback__current-slide');
+    targetSlide.classList.add('feedback__current-slide');
+}
+
+const updateDots = (currentDot, targetDot) => {
+    currentDot.classList.remove('active');
+    targetDot.classList.add('active');
+}
+
+const showHideArrows = (slides, prevIcon, nextIcon, targetIndex) => {
+    if(targetIndex === 0) {
+        prevIcon.classList.add('is-hidden');
+        nextIcon.classList.remove('is-hidden');
+    } else if(targetIndex === dotsArray.length-1){
+        prevIcon.classList.remove('is-hidden');
+        nextIcon.classList.add('is-hidden');
+    } else {
+        prevIcon.classList.remove('is-hidden');
+        nextIcon.classList.remove('is-hidden');
+    }
+}
+
+
+
+// Set slide position
+const setSlidePosition = (slide, index) => {
+    slide.style.left = index * slideWidth + 'px';
+}
+
+slides.forEach(setSlidePosition);
+
+// Move slide to the left
+prevIcon.addEventListener('click', () => {
+
+    const currentSlide = track.querySelector('.feedback__current-slide');
+    const prevSlide = currentSlide.previousElementSibling;
+    const currentDot = dotsNav.querySelector('.active');
+    const prevDot = currentDot.previousElementSibling;
+    const prevIndex = slides.findIndex(slide => slide === prevSlide);
+   
+    moveToSlide(track, currentSlide, prevSlide);
+    updateDots(currentDot, prevDot);
+    showHideArrows(slides, prevIcon, nextIcon, prevIndex);
+
+});
+
+
+// Move slide to the right
+nextIcon.addEventListener('click', () => {
+
+    const currentSlide = track.querySelector('.feedback__current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const currentDot = dotsNav.querySelector('.active');
+    const nextDot = currentDot.nextElementSibling;
+    const nextIndex = slides.findIndex(slide => slide === nextSlide);
+
+    moveToSlide(track, currentSlide, nextSlide)
+    updateDots(currentDot, nextDot);
+    showHideArrows(slides, prevIcon, nextIcon, nextIndex);
+
+});
+
+
+// Move to target slide when click dot
+dotsNav.addEventListener('click',(e) => {
+
+    const currentSlide = track.querySelector('.feedback__current-slide');
+    const currentDot = dotsNav.querySelector('.active');
+    const targetDot = e.target.closest('div');
+    const targetIndex = dotsArray.findIndex(dot => dot === targetDot);
+    const targetSlide = slides[targetIndex];
+
+    moveToSlide(track, currentSlide, targetSlide);
+    updateDots(currentDot, targetDot);
+    showHideArrows(slides, prevIcon, nextIcon, targetIndex);
+
+})
+
+
+
+
+
